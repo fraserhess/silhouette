4e50d9ed38f7782e886acb927609a113815048f1
Fraser Hess <fraser@sweeterrhythm.com>
2013-10-17 19:39:05 -0600

    Initial Commit

---
+++ Silhouette/Silhouette.m
@@ -0,0 +1,13 @@
+//
+//  Silhouette.m
+//  Silhouette
+//
+//  Created by Fraser Hess on 10/17/13.
+//  Copyright (c) 2013 Glencode LLC. All rights reserved.
+//
+
+#import "Silhouette.h"
+
+@implementation Silhouette
+
+@end
